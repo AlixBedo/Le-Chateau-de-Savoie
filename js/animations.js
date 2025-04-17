31feb86c9460f8d980b9f7491b489492ee9bf830
AlixBedo <164504456+AlixBedo@users.noreply.github.com>
2025-04-17 03:34:55 +0200

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.add('hidden');
+        observer.observe(section);
+    });
+});
+
+
